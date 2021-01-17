a0a2c5f6db4db197f966c050018feb5c9ec7f4d1
Nik Kurent <nik.kurent1@gmail.com>
2021-01-17 22:16:22 +0100

    setting .env

--- dogodki/settings.py
+++ dogodki/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import environ
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['Events_secret_key']
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  False
+DEBUG =  config('DEBUG', cast=bool, default=True)
 ALLOWED_HOSTS = ['glasbeni-dogodki.herokuapp.com', '127.0.0.1']
 
 
