94361d01cbd325e724e8da7cc697f5d776f2f014
Nik Kurent <nik.kurent1@gmail.com>
2021-01-17 21:55:07 +0100

    commit

--- dogodki/settings.py
+++ dogodki/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('Events_secret_key')
+SECRET_KEY = os.environ['Events_secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  False
